ea8c77eb903e37eb994866e36d832450e58f427c
Jiwoo Seo <sjwoos123@gmail.com>
2025-08-19 10:31:32 +0900

    querydsl

--- backend/src/main/kotlin/com/back/domain/member/member/repository/MemberRepositoryCustom.kt
+++ backend/src/main/kotlin/com/back/domain/member/member/repository/MemberRepositoryCustom.kt
@@ -1,4 +1,6 @@
 package com.back.domain.member.member.repository
+import com.back.domain.member.member.entity.Member
 
 interface MemberRepositoryCustom {
+    fun findQById(id: Int): Member?
 }
--- backend/src/main/kotlin/com/back/domain/member/member/repository/MemberRepositoryImpl.kt
+++ backend/src/main/kotlin/com/back/domain/member/member/repository/MemberRepositoryImpl.kt
@@ -1,5 +1,21 @@
 package com.back.domain.member.member.repository
+import com.back.domain.member.member.entity.Member
+import com.back.domain.member.member.entity.QMember
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.stereotype.Repository
 
-class MemberRepositoryImpl: MemberRepositoryCustom {
+@Repository
+class MemberRepositoryImpl(
+    private val queryFactory: JPAQueryFactory
+) : MemberRepositoryCustom {
 
+    override fun findQById(id: Int): Member? {
+        val member = QMember.member
+
+        return queryFactory
+            .select(member)
+            .from(member)
+            .where(member.id.eq(id))
+            .fetchOne()
+    }
 }
